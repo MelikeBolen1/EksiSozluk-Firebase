a6eea3ac04f362e7eb578eefa11c3cc1ddc156a4
Melike <melikebolen1@gmail.com>
2022-06-25 10:55:57 +0300

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Melike Bolen on 6/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
